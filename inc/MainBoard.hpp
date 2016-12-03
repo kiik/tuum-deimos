a2ba6bfb74c638281e56985838b7b44b7367fbcc
Helen Asukula <asukyla@windowslive.com>
2016-12-03 14:22:53 +0200

    Update dribbler working speed.

--- inc/MainBoard.hpp
+++ inc/MainBoard.hpp
@@ -37,7 +37,7 @@ namespace tuum { namespace hal {
     void senseBall();
     void chargeCoil();
     void releaseCoil();
-    void startDribbler(float = 0.1);
+    void startDribbler(float = 0.2);
     void stopDribbler();
 
     void coilKick();
